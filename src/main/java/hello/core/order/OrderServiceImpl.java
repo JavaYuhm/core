f903e757a5b91efd1b02f556f84a38aa83b30a61
JavaYuhm <kora47461381@gmail.com>
2022-09-29 22:02:59 +0900

    [Feat] DIP, OCP 관련 내용 : 다형성을 통해서 인터페이스로 구현을 하였으나 클라이언트에서 구체(구현체)를 직접 선택하는 문제가 있엇음 이를 AppConfig 외부에서 클라이언트에 생성자 주입을 한다(DI) 20220929

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -2,14 +2,22 @@ package hello.core.order;
 
 import hello.core.discount.DicsountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
 
 public class OrderServiceImpl implements  OrderService{
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
-    private final DicsountPolicy dicsountPolicy = new FixDiscountPolicy();
+    private final MemberRepository memberRepository;
+    private final DicsountPolicy dicsountPolicy;
+    //private final DicsountPolicy dicsountPolicy = new FixDiscountPolicy();
+    //private final DicsountPolicy dicsountPolicy = new RateDiscountPolicy();
+
+    public OrderServiceImpl(MemberRepository memberRepository, DicsountPolicy dicsountPolicy){
+        this.memberRepository = memberRepository;
+        this.dicsountPolicy = dicsountPolicy;
+    }
 
     @Override
     public Order crateOrder(Long memberId, String itemName, int itemPrice) {
