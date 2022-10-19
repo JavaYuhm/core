5954c9cd3ee6085d00b5293dd8231eaeb142e71e
JavaYuhm <kora47461381@gmail.com>
2022-10-19 22:49:34 +0900

    [Feat] 스프링 AppConfig 리팩토링 및 스프링 컨테이너에 bean으로 등록 / Test Code 에 스프링 컨테이너정보 , Bean 조회 테스트 작성 20221019

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -1,5 +1,7 @@
 package hello.core;
 
+import hello.core.discount.DicsountPolicy;
+import hello.core.discount.FixDiscountPolicy;
 import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
@@ -7,16 +9,39 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
+    // 역할과 구현으로 분리
+    // 다른 구현체로 쉽게 변경할 수 있음. 어떤역할을 하는 지 보임.
+    // 애플리케이션 전체 구성이 어떻게 되어 있는 지 파악 가능함.
 
+    @Bean
     public MemberService memberService(){
         // 생성자 주입
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
     }
 
+    @Bean
+    public MemberRepository memberRepository(){
+        return new MemoryMemberRepository();
+    }
+
+    @Bean
     public OrderService orderService(){
-        return new OrderServiceImpl(new MemoryMemberRepository(), new RateDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), dicsountPolicy());
     }
 
+    @Bean
+    public DicsountPolicy dicsountPolicy(){
+/*
+        Appconfig 변경으로 할인 정책 변경 가능함.
+        사용영역의 어떤 코드도 변경 X, 구성영역은 당연히 변경됨.
+        AppConfig 애플리케이션의 공연 기획자로 생각하자.
+        return  new FixDiscountPolicy();
+*/
+        return new RateDiscountPolicy();
+    }
 }
--- src/main/java/hello/core/MemberApp.java
+++ src/main/java/hello/core/MemberApp.java
@@ -4,12 +4,20 @@ import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
+/*        AppConfig appConfig = new AppConfig();
+        MemberService memberService = appConfig.memberService();*/
         //MemberService memberService = new MemberServiceImpl();
+
+        // 스프링 객체를 관리해줌. AppConfig의 설정 정보를 스프링 컨테이너에 생성함.
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+
+
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
 
